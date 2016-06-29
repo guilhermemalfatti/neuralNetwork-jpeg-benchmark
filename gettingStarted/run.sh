b699e8c22a0e6b1b6a53e71c2eb7e04df9b1d506
malfatti <malfatti@hp.com>
2016-06-29 16:43:56 -0300

    added run file

---
+++ gettingStarted/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+green='\033[0;32m'
+nc='\033[0m' # No Color
+
+echo -ne "${green}$f\t"
+echo "start training"
+echo -ne "${nc}"
+g++ -O3 -std=c++11 training.c -o training -lfann -lm
+
+./training
+echo -ne "${green}$f\t"
+echo "start the execution"
+echo -ne "${nc}"
+g++ -O3 -std=c++11 execution.c -o execution -lfann -lm
+
+./execution
+
