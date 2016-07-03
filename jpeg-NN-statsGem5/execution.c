814e1f7c37ef2793f561a129e7711ff30f938e03
malfatti <guimalfatti76@gmail.com>
2016-07-03 20:47:31 -0300

    fixed the inputs

--- jpeg-NN-statsGem5/execution.c
+++ jpeg-NN-statsGem5/execution.c
@@ -1,22 +1,37 @@
 /*
 compile:
-	g++ -O3 -std=c++11 execution.c -o execution -lfann -lm -static /mnt/gmalfatti/git/gem5-stable/util/m5/m5op_x86.S  -fopenmp
+	g++ -O3 -std=c++11 execution.c -o execution -lfann -lm -static /home/malfatti/Documents/git/gem5-stable/util/m5/m5op_x86.S  
 
 execution:
-
+    ./build/X86/gem5.fast configs/example/se.py  --cpu-type=detailed --caches --l2cache --l1d_size='32kB' --l1i_size='32kB' --l2_size='1MB' --cmd /home/malfatti/Documents/git/neuralNetwork-jpeg-benchmark/jpeg-NN-statsGem5/execution
 */
 #include <stdio.h>
 #include "floatfann.h"
-#include </mnt/gmalfatti/git/gem5-stable/util/m5/m5op.h>
+//#include </mnt/gmalfatti/git/gem5-stable/util/m5/m5op.h>
+#include </home/malfatti/Documents/git/gem5-stable/util/m5/m5op.h>
 
 int main()
 {
     fann_type *calc_out;
     fann_type input[64];
 
+    input[0] = -0.015625; input[1] = 0.015625; input[2] = 0.0195312; input[3] = 0.015625; input[4] = 0.015625;
+    input[5] = 0.0195312; input[6] = 0.0117188; input[7] = 0.0234375; input[8] = 0.0234375; input[9] = 0.0195312;
+    input[10] = 0.015625; input[11] = 0.0195312; input[12] = 0.0195312; input[13] = 0.015625; input[14] = 0.0195312;
+    input[15] = -0.0195312; input[16] = 0.0195312; input[17] = 0.0195312; input[18] = 0.015625;  input[19] = 0.0234375;
+    input[20] = 0.0234375; input[21] = 0.0195312; input[22] = 0.0195312; input[23] =-0.0195312; input[24] = 0.015625;
+    input[25] = 0.015625; input[26] = 0.0195312; input[27] = 0.015625; input[28] = 0.0195312; input[29] = 0.0234375;
+    input[30] = 0.015625; input[31] = -0.0195312; input[32] = 0.015625; input[33] = 0.015625; input[34] = 0.015625;
+    input[35] = 0.0195312; input[36] = 0.0195312; input[37] = 0.0234375; input[38] = 0.0195312; input[39] = -0.015625;
+    input[40] = 0.015625; input[41] = 0.0273438; input[42] = 0.0195312; input[43] = 0.0195312; input[44] = 0.0195312;
+    input[45] = 0.0195312; input[46] = 0.0195312; input[47] = -0.015625; input[48] = 0.0195312; input[49] = 0.0234375;
+    input[50] = 0.015625; input[51] = 0.0117188; input[52] = 0.0234375; input[53] = 0.0195312; input[54] = 0.0117188;
+    input[55] = -0.015625; input[56] = 0.015625; input[57] = 0.015625; input[58] = 0.0234375; input[59] = 0.0195312;
+    input[60] = 0.015625; input[61] = 0.015625; input[62] = 0.0195312; input[63] = 0.0195312;
+
+    
+    struct fann *ann = fann_create_from_file("/home/malfatti/Documents/git/neuralNetwork-jpeg-benchmark/jpeg-NN-statsGem5/inOutJpeg.net");
     m5_reset_stats(1,0);
-    struct fann *ann = fann_create_from_file("inOutJpeg.net");
-    input[64] = (-0.015625, -0.015625, -0.0195312, -0.015625, -0.015625, -0.0195312, -0.0117188, -0.0234375, -0.0234375, -0.0195312, -0.015625, -0.0195312, -0.0195312, -0.015625, -0.0195312, -0.0195312, -0.0195312, -0.0195312, -0.015625, -0.0234375, -0.0234375, -0.0195312, -0.0195312, -0.0195312, -0.015625, -0.015625, -0.0195312, -0.015625, -0.0195312, -0.0234375, -0.015625, -0.0195312, -0.015625, -0.015625, -0.015625, -0.0195312, -0.0195312, -0.0234375, -0.0195312, -0.015625, -0.015625, -0.0273438, -0.0195312, -0.0195312, -0.0195312, -0.0195312, -0.0195312, -0.015625, -0.0195312, -0.0234375, -0.015625, -0.0117188, -0.0234375, -0.0195312, -0.0117188, -0.015625, -0.015625, -0.015625, -0.0234375, -0.0195312, -0.015625, -0.015625, -0.0195312, -0.0195312);
     calc_out = fann_run(ann, input);
     m5_dump_stats(1,0);	
 
--- jpeg-NN-statsGem5/training.c
+++ jpeg-NN-statsGem5/training.c
@@ -1,6 +1,7 @@
 /*
 compile:
     g++ -O3 -std=c++11 training.c -o training -lfann -lm
+    g++ -O3 -std=c++11 training.c -o training -lfann -lm -static /home/malfatti/Documents/git/gem5-stable/util/m5/m5op_x86.S
 
 execution:
 
@@ -10,6 +11,8 @@ if fann dont work, we need link the libraries
     
 */
 #include "fann.h"
+//#include </mnt/gmalfatti/git/gem5-stable/util/m5/m5op.h>
+#include </home/malfatti/Documents/git/gem5-stable/util/m5/m5op.h>
 
 int main()
 {
@@ -20,17 +23,21 @@ int main()
     const float desired_error = (const float) 0.001;
     const unsigned int max_epochs = 200;
     const unsigned int epochs_between_reports = 1000;
-
+    
+    m5_reset_stats(1,0);
     struct fann *ann = fann_create_standard(num_layers, num_input,
         num_neurons_hidden, num_output);
 
     fann_set_activation_function_hidden(ann, FANN_SIGMOID_SYMMETRIC);
     fann_set_activation_function_output(ann, FANN_SIGMOID_SYMMETRIC);
 
-    fann_train_on_file(ann, "inOutJpeg-training.data", max_epochs,
+    //fann_train_on_file(ann, "/mnt/gmalfatti/git/neuralNetwork-jpeg-benchmark/jpeg-NN-statsGem5/inOutJpeg-training.data", max_epochs,
+    fann_train_on_file(ann, "/home/malfatti/Documents/git/neuralNetwork-jpeg-benchmark/jpeg-NN-statsGem5/inOutJpeg-training.data", max_epochs,
         epochs_between_reports, desired_error);
 
-    fann_save(ann, "inOutJpeg.net");
+    //fann_save(ann, "/mnt/gmalfatti/git/neuralNetwork-jpeg-benchmark/jpeg-NN-statsGem5/inOutJpeg.net");
+    fann_save(ann, "/home/malfatti/Documents/git/neuralNetwork-jpeg-benchmark/jpeg-NN-statsGem5/inOutJpeg.net");
+    m5_dump_stats(1,0); 
 
     fann_destroy(ann);
 
